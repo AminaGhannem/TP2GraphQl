e33dc9dab62177936c117e1dd5e3b98d91402d90
AminaGhannem <amina.ghannem@insat.ucar.tn>
2024-04-20 09:39:36 +0200

    creating prisma schema

---
+++ src/script.ts
@@ -0,0 +1,18 @@
+// 1
+import { PrismaClient } from '@prisma/client'
+ 
+// 2
+const prisma = new PrismaClient()
+ 
+// 3
+async function main() {
+  const allLinks = await prisma.link.findMany()
+  console.log(allLinks)
+}
+ 
+// 4
+main()
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
